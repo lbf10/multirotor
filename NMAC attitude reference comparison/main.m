89d5034b76e053729a9183ca0bf1aefd851873fa
lbf10 <leonardolbf10@gmail.com>
2018-06-11 22:11:30 -0300

    Changed crossover rate to 0.5 and mutation to uniform with rate 0.05. testing with PID again.

---
+++ NMAC attitude reference comparison/main.m
@@ -0,0 +1,13 @@
+% Multirotors architectures simulation for NMAC comparison
+% refer to "doc multicontrol" or "doc multicopter" for more information
+
+addpath(genpath('../'))
+warning('on','all')
+clear all
+clc
+
+%% Multirotor configuration
+armWeight = ;
+rotorInertia = ;
+liftCoeff = ;
+dragCoeff = ;
