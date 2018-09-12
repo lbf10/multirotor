819b6a39d43b6288cb44addfb264543ac0713014
lbf10 <leonardolbf10@gmail.com>
2018-09-11 23:02:41 -0300

    changed to SOSMC Passive NMAC with PIDD

--- ag_with_estimated_model/main.m
+++ ag_with_estimated_model/main.m
@@ -6,7 +6,7 @@ addpath(genpath('../multiControl/'))
 warning('off','all')
 
 %% Algorithms to train
-attitudeController = 'SOSMC Passive';
+attitudeController = 'SOSMC Passive with PIDD';
 controlAllocator = 'Passive NMAC';
 attitudeReference = 'Passive NMAC';
 
@@ -170,8 +170,8 @@ fullfilename = 0;
             alpha = 2*[1,1,1,2,2,2];
             lambda = .1*[1,1,1,2,2,2];
             initialPopulation = [initialPopulation,c,lambda,alpha];
-            lb = [lb,-inf(1,18)];
-            ub = [ub,inf(1,18)];
+            lb = [lb,-100*ones(1,18)];
+            ub = [ub,100*ones(1,18)];
             nvars = 30;
             initialPopulation = [initialPopulation,0,0,0.5];
             lb = [lb,0,0,0];
