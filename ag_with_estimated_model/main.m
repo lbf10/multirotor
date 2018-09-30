86051fb5065748b4368928ca3a0c87cc380ac9c8
lbf10 <leonardolbf10@gmail.com>
2018-09-30 15:20:11 -0300

    payload changed to 3kg to consider cases that will be able to finish simulation. CHanged stall gen limit to 25 because 50 is too much. Changed number of generations to 300 and population to 1300 to give approx. 45h of simulation. Changed fitness toleration to 1e-5.

--- ag_with_estimated_model/main.m
+++ ag_with_estimated_model/main.m
@@ -372,8 +372,8 @@ fitnessfcn = @(x) controlFitness(attitudeController, controlAllocator, attitudeR
 filename = ['Results/',attitudeController,'_',controlAllocator,'_',attitudeReference,'_',datestr(now),'_result.mat'];
 iterFilename = ['Results/',attitudeController,'_',controlAllocator,'_',attitudeReference,'_',datestr(now),'_iterations.mat'];
 outFunction = @(options,state,flag) saveIter(options,state,flag,iterFilename);
-options = gaoptimset('PopulationSize',1000,'Generations',200,'Display','iter','InitialPopulation',initialPopulation,'OutputFcn',outFunction,'Vectorized','on','CrossoverFraction',0.5,'MutationFcn', {@mutationuniform, 0.05});
-poolobj = parpool(2);
+options = gaoptimset('PopulationSize',1300,'Generations',300,'Display','iter','InitialPopulation',initialPopulation,'OutputFcn',outFunction,'Vectorized','on','CrossoverFraction',0.5,'MutationFcn', {@mutationuniform, 0.05}, 'StallGenLimit',25,'TolFun',1e-5);
+poolobj = parpool(80);
 addAttachedFiles(poolobj,{'controlFitness.m','saveIter.m','paramsToMultirotor.m','../multiControl/'})
 [bestIndividual,bestFitness, EXITFLAG,OUTPUT,POPULATION,SCORES] = ga(fitnessfcn,nvars,[],[],[],[],lb,ub,[],options);
 delete(poolobj)
