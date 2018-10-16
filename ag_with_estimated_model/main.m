da484dee58001014a60f3dda3b71aaf536dfd3b3
lbf10 <leonardolbf10@gmail.com>
2018-10-16 08:36:48 -0300

    changed population to 3000 to see if SOSMC gets better

--- ag_with_estimated_model/main.m
+++ ag_with_estimated_model/main.m
@@ -372,7 +372,7 @@ fitnessfcn = @(x) controlFitness(attitudeController, controlAllocator, attitudeR
 filename = ['Results/',attitudeController,'_',controlAllocator,'_',attitudeReference,'_',datestr(now),'_result.mat'];
 iterFilename = ['Results/',attitudeController,'_',controlAllocator,'_',attitudeReference,'_',datestr(now),'_iterations.mat'];
 outFunction = @(options,state,flag) saveIter(options,state,flag,iterFilename);
-options = gaoptimset('PopulationSize',1500,'Generations',300,'Display','iter','InitialPopulation',initialPopulation,'OutputFcn',outFunction,'Vectorized','on','CrossoverFraction',0.5,'MutationFcn', {@mutationuniform, 0.05}, 'StallGenLimit',25);
+options = gaoptimset('PopulationSize',3000,'Generations',300,'Display','iter','InitialPopulation',initialPopulation,'OutputFcn',outFunction,'Vectorized','on','CrossoverFraction',0.5,'MutationFcn', {@mutationuniform, 0.05}, 'StallGenLimit',25);
 poolobj = parpool(80);
 addAttachedFiles(poolobj,{'controlFitness.m','saveIter.m','paramsToMultirotor.m','../multiControl/'})
 [bestIndividual,bestFitness, EXITFLAG,OUTPUT,POPULATION,SCORES] = ga(fitnessfcn,nvars,[],[],[],[],lb,ub,[],options);
