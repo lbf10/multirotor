4b02109803802d6d03682f60112007232d41e545
lbf10 <leonardolbf10@gmail.com>
2019-05-30 22:20:21 -0300

    corrected evluation scripts for merheb model

--- ag_with_merheb_model/evaluateController.m
+++ ag_with_merheb_model/evaluateController.m
@@ -28,7 +28,7 @@ samples = load('../monteCarloSamples/monteCarlo_samples.mat');
 samplesFields = fields(samples);
 
 for jt = 1:numel(samplesFields)
-    if ~strcmp(samplesFields{jt},'columnNames')
+    if ~strcmp(samplesFields{jt},'columnNames') && ~strcmp(samplesFields{jt},'N4')
         disp([samplesFields{jt},' - Starting samples matrix ',datestr(now)])
         options = samples.(samplesFields{jt});
         options(1,end+1) = {0};
@@ -74,52 +74,44 @@ for jt = 1:numel(samplesFields)
             multirotor.setRotorOperatingPoint(1:8,352*[1 1 1 1 1 1 1 1]);
             multirotor.configControlAllocator('Passive NMAC',1,0);
             multirotor.configControlAllocator('Active NMAC',1,0);
-            multirotor.setTimeStep(0.005);
-            controlLoopTime = option{6};
-            multirotor.setControlTimeStep(controlLoopTime);
-            multirotor.configFDD(0.9,0.5);
-            multirotor.setSimEffects('motor dynamics off','solver euler')
-            multirotor.setControlDelay(0.20);
+            multirotor.setTimeStep(0.002);
+            multirotor.setControlTimeStep(option{6});
+            multirotor.configFDD(0.99,0.2);
+            multirotor.setSimEffects('motor dynamics on','solver ode45')
+            multirotor.setControlDelay(option{7});
             multirotor = paramsToMultirotor(attitudeController, controlAllocator, attitudeReference, multirotor, bestIndividual);
             
-            
-            endTime = option{1};
-            [waypoints, time] = geronoToWaypoints(7, 4, 4, endTime, endTime/8, 'goto',option{2});
+
+            endTime = option{2};
+            [waypoints, time] = geronoToWaypoints(7, 4, 4, endTime, endTime/8, 'goto',2*pi);
             multirotor.setTrajectory('waypoints',waypoints,time);
-            payload = option{3};
-            position = payload(2:4);
-            mass = payload(1);
+            position = [0,0,-payloadRadius];
+            mass = option{1}*multirotor.mass();
             multirotor.setPayload(position,mass,eye(3)*2*mass*payloadRadius*payloadRadius/5);
             crossTime1 = num2str(endTime/4);
             crossTime2 = num2str(3*endTime/4);
-            disturbance = option{4};
+            disturbance = option{3}*multirotor.mass();
             if disturbance ~= 0
                 multirotor.setLinearDisturbance(['@(t) [0;1;0]*',num2str(disturbance),'*(exp(-(t-',crossTime1,')^2/(0.5))+exp(-(t-',crossTime2,')^2/(0.5)))']);
             end
             failure = option{5};
             nFails = length(failure);
             step = endTime/2/(1+nFails);
-            %         multirotor.addCommand(failure,step:step:endTime/2-0.000001);
             multirotor.addCommand(failure,endTime/2+step:step:endTime-0.000001);
-            
             try
                 multirotor.run('visualizeGraph',false,'visualizeProgress',false,'metricPrecision',0.15,'angularPrecision',5);
-                metrics = multirotor.metrics();
-                option{7} = 1*(1-metrics.simulationSuccess)+metrics.RMSPositionError+real(metrics.RMSAngularError)+metrics.RMSPower/500;
+                metrics = multirotor.metrics();          
+                option{8} = 1*(1-metrics.simulationSuccess)+metrics.RMSPositionError+real(metrics.RMSAngularError)+metrics.RMSPower/500;
             catch
-                option{7} = 10;
+                option{8} = 10;
             end
-            option{8} = multirotor.metrics();
+            option{9} = multirotor.metrics();
             options(it,:) = option;
-            %     multirotor.save('all',[pathname,foldername,'/multirotorSimulation_',num2str(it),'.mat']);
-            %         multirotor.plotSim()
-            %         multirotor.metrics()
-            %         pause
-            
-            disp(['Finished calculation ',num2str(it)])
+%            disp(['Finished calculation ',num2str(it)])
         end
-        save([pathname,'/',foldername,'/evaluationResult.mat'],'options');
-        disp(['Evaluation finished.',datestr(now)])
+        samples.(samplesFields{jt}) = options;
+        disp([samplesFields{jt},' - Finished samples matrix ',datestr(now)])
+        save([pathname,'/',foldername,'/evaluationResult.mat'],'samples');
     end
 end
 end
