d4503972c3152c53959951202204e55d65b46cfe
lbf10 <leonardolbf10@gmail.com>
2019-06-23 14:15:35 -0300

    trying to evaluate merheb. 1500 samples, ode45, 2ms

--- ag_with_merheb_model/callMerhebEvaluation.m
+++ ag_with_merheb_model/callMerhebEvaluation.m
@@ -3,11 +3,12 @@ addpath('../multiControl/')
 addpath('../multiControl/utils')
 warning('off','all')
 
-if  isempty(gcp('nocreate'))
+% if  isempty(gcp('nocreate'))
     poolobj = parpool('ClusterPandora',68);
 %     poolobj = parpool('local',2);
     addAttachedFiles(poolobj,{'evaluateMerhebController.m','paramsToMultirotor.m','../multiControl/@multicopter/multicopter.m','../multiControl/@multicopter/model.m','../multiControl/@multicontrol/multicontrol.m'})
-end
+    updateAttachedFiles(poolobj);
+    % end
 
 filelist = dir('Results/Best');
 filelist(1:2) = [];
