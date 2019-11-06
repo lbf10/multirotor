69e5763385988644e90a74bb1a64980b785d4971
lbf10 <leonardolbf10@gmail.com>
2019-11-06 20:54:20 -0300

    starting new passive markovian evaluation

--- ag_with_merheb_model/callMerhebEvaluation.m
+++ ag_with_merheb_model/callMerhebEvaluation.m
@@ -10,12 +10,15 @@ warning('off','all')
     updateAttachedFiles(poolobj);
     % end
 
-filelist = dir('Results/Best');
-filelist(1:2) = [];
-
-for it=1:length(filelist)
-    if ~filelist(it).isdir
-        filename = ['Results/Best/',filelist(it).name];
-        evaluateMerhebController(filename);
-    end
-end
+    filename = 'Results/2. 15 end time with 0.05 mr and 4 sequential failures/Markoviano_com_P/Markovian RLQ-R Passive Modified_Passive NMAC_Passive NMAC_01-Nov-2019 00:02:47_iterations.mat';
+    evaluateMerhebController(filename);
+    
+% filelist = dir('Results/Best');
+% filelist(1:2) = [];
+% 
+% for it=1:length(filelist)
+%     if ~filelist(it).isdir
+%         filename = ['Results/Best/',filelist(it).name];
+%         evaluateMerhebController(filename);
+%     end
+% end
